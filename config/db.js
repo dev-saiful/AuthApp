479b6ac6097283479384e95b3f16b2e05fc03a46
dev-saiful <saifulcseian@gmail.com>
2023-10-30 22:28:02 +0600

    database connection functionality added

--- config/db.js
+++ config/db.js
@@ -2,6 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config();
 
+
+// database connection establish checking
 exports.connect = ()=>{
-mongoose.connect();
+mongoose.connect(process.env.MONGODB_URL,
+    {
+        useNewUrlParser:true,
+        useUnifiedTopolog:true,
+    })
+    .then(()=>{
+        console.log("Database connected successfully!");
+    })
+    .catch((err)=>{
+        console.log("Database connection failed");
+        console.error(err);
+        process.exit(1);
+    })
 }
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
+import {connect} from "./config/db";
 const app = express();
 const PORT = process.env.PORT || 4005;
 // parsing data from request body
 app.use(express.json());
+// database function call
+connect();
+
