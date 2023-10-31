90d0821681bb4c200b8dc230c66738d3fe73165c
dev-saiful <saifulcseian@gmail.com>
2023-10-31 06:20:18 +0600

    user route import and mount

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import {connect} from "./config/db";
+import { user } from "./routes/user";
+
+
 const app = express();
 const PORT = process.env.PORT || 4005;
 // parsing data from request body
@@ -9,3 +12,9 @@ app.use(express.json());
 // database function call
 connect();
 
+// user route mount
+app.use("/api/v1",user);
+
+app.listen(PORT,()=>{
+    console.log(`localhost:${PORT}`);
+});
