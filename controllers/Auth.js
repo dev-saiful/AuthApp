4b4eafc1db49cd85b874c5b1328c983516792993
dev-saiful <saifulcseian@gmail.com>
2023-11-01 12:24:39 +0600

    login functionality done

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import {userModel} from "../models/User.model.js";
+import jwt from "jsonwebtoken";
 
 // signup routes handler
 export const signup = async(req,res)=>{
@@ -54,5 +55,77 @@ export const signup = async(req,res)=>{
 // login 
 
 export const login = async(req,res)=>{
-    
+    try 
+    {
+            // getting user info
+        const {email,password} = req.body;
+        // check empty
+        if(!email || !password)
+        {
+            return res.status(400).json({
+                success:false,
+                message:"Fill all input",
+            });
+        }
+
+        let user = await userModel.findOne({email});
+        // email not exists
+        if(!user)
+        {
+            return res.status(401).json({
+                success:false,
+                message:"User is not register",
+            });
+        }
+
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        };
+
+        // check password is matche or not
+        // let token;
+        if(await bcrypt.compare(password,user.password))
+        {
+            // password match
+           const token = jwt.sign(payload,process.env.JWT_SECRECT,{
+                expiresIn:"2h",
+            });
+            user = user.toObject();
+           user.token = token;
+            user.password = undefined;
+           
+
+            const options = {
+                expires: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true,
+            }
+
+            //  creating cookie
+            res.cookie("token",token,options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:"User Logged in Successfully",
+            });
+        }
+        else
+        {
+            // password mismatch
+            return res.status(403).json({
+                success:false,
+                message:"Password Mismatch",
+            });
+        }
+    } 
+    catch (error) 
+    {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Login Failed",
+        });
+    }
+
 }
