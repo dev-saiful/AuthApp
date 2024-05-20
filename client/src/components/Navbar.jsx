71d703459733401c684abfe46bea093ae860d304
dev-saiful <saifulcseian@gmail.com>
2024-05-20 12:40:43 +0600

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,41 @@
-import { Link,NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-   <nav className="bg-purple-500 flex flex-wrap items-center justify-between h-16">
-    <Link className="mx-10" to="/">AuthApp</Link>
-    <div className="mx-10 h-full w-1/3 py-5">
-    <NavLink className=" bg-yellow-400 m-1" to="/">Home</NavLink>
-    <NavLink className=" bg-yellow-400 m-1" to="/login">Login</NavLink>
-    <NavLink className=" bg-yellow-400 m-1" to="/register">Register</NavLink>
-    </div>
-   </nav>
-
-  )
+    <nav className=" bg-teal-300">
+      <div className="relative w-[1080px] mx-auto flex items-center justify-between">
+        <Link to="/">
+          <img 
+          src="./public/vite.svg" alt="logo"
+          className="cursor-pointer py-7 pr-7"
+           />
+        </Link>
+        <ul className="flex gap-4">
+        <li className=" py-7 hover:text-orange-400 cursor-pointer transition-all duration-200 relative group">
+          <NavLink  to="/">
+            Home
+          </NavLink>
+          <div className=" absolute bottom-0 w-full h-1 bg-orange-400 hidden group-hover:block transition-all duration-200"></div>
+        </li>
+        <li className=" py-7 hover:text-orange-400 cursor-pointer transition-all duration-200 relative group">
+          <NavLink  to="/login">
+            Login
+          </NavLink>
+          <div className=" absolute bottom-0 w-full h-1 bg-orange-400 hidden group-hover:block transition-all duration-200"></div>
+        </li>
+        <li className=" py-7 hover:text-orange-400 cursor-pointer transition-all duration-200 relative group">
+          <NavLink  to="/register">
+            Register
+          </NavLink>
+          <div className=" absolute bottom-0 w-full h-1 bg-orange-400 hidden group-hover:block transition-all duration-200"></div>
+        </li>
+        </ul>
+        <div className="flex gap-4">
+          <button className="py-3 px-5 bg-orange-400 border rounded-sm text-sm">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
-
-
+export default Navbar;
